9a16724329638deff128966f3cda52a6ab708dea
Arjen <arjen.chen@mantle.xyz>
2026-03-02 17:53:35 +0800

    feat: update approveToAdapter

--- src/vault/interfaces/IMantleYieldVault.sol
+++ src/vault/interfaces/IMantleYieldVault.sol
@@ -108,7 +108,7 @@ interface IMantleYieldVault is IERC4626, IERC7540Redeem {
     event MinRedeemAmountUpdated(uint256 oldAmount, uint256 newAmount);
     event AdapterRegistered(address indexed adapter);
     event AdapterRemoved(address indexed adapter);
-    event AdapterApproved(address indexed adapter, uint256 amount);
+    event AdapterApproved(address indexed adapter, address indexed token, uint256 amount);
     event RequestBatchUpdated(uint256[] ids, RequestStatus newStatus);
     event InFlightCreated(
         uint256 indexed inFlightId,
@@ -218,7 +218,7 @@ interface IMantleYieldVault is IERC4626, IERC7540Redeem {
 
     function registerAdapter(address adapter) external;
     function removeAdapter(address adapter) external;
-    function approveToAdapter(address adapter, uint256 amount) external;
+    function approveToAdapter(address adapter, address token, uint256 amount) external;
     function updateRequestBatch(uint256[] calldata ids, RequestStatus newStatus) external;
     function markRequestsReady(uint256[] calldata ids) external;
     function createInFlight(address adapter, address asset, uint256 tokenAmount, uint256 usdcAmount, bool isInvest)
