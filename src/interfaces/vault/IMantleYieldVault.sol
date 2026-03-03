87876b13b002bc20cf9b1606a4edf6de37504472
Luke Long <yin.long@mantle.xyz>
2026-03-03 14:15:45 +0800

    feat: add ISanctionsOracle

--- src/interfaces/vault/IMantleYieldVault.sol
+++ src/interfaces/vault/IMantleYieldVault.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ISanctionsOracle} from "../ISanctionsOracle.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-
-// =============================================================
-// External Interface Definitions
-// =============================================================
-
-interface ISanctionsOracle {
-    function isBlacklisted(address account) external view returns (bool);
-}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // =============================================================
 // ERC-7540 Interface Definition
