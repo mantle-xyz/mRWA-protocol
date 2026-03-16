eaf2e8873e03f0d1dea37891f7ec06f73ab30cb4
Adam <adam.xu@mantle.xyz>
2026-03-16 16:00:34 +0800

    forge fmt

--- src/protocol/StrategyController.sol
+++ src/protocol/StrategyController.sol
@@ -302,13 +302,9 @@ contract StrategyController is Initializable, AccessControlUpgradeable, Reentran
         }
     }
 
-    function _applyStrategyUpdate(
-        address adapter,
-        uint16 targetWeightBps,
-        uint16 priority,
-        bool isAsync,
-        bool isActive
-    ) internal {
+    function _applyStrategyUpdate(address adapter, uint16 targetWeightBps, uint16 priority, bool isAsync, bool isActive)
+        internal
+    {
         if (targetWeightBps > BPS_DENOMINATOR) {
             revert InvalidBps();
         }
@@ -545,8 +541,7 @@ contract StrategyController is Initializable, AccessControlUpgradeable, Reentran
     // =============================================================
 
     function _invest(uint256 excessCash) internal {
-        uint256 totalAssets =
-            asset.balanceOf(address(vault)) + _totalStrategyValue() + vault.totalInvestInFlight()
+        uint256 totalAssets = asset.balanceOf(address(vault)) + _totalStrategyValue() + vault.totalInvestInFlight()
             + vault.totalRedeemInFlight();
         uint256 requested = excessCash;
         uint256 remaining = excessCash;
