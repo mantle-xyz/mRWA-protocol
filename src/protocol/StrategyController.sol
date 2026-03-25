17aa6a16b7782a686f6ebe2a17e9858b5e191252
Arjen <arjen.chen@mantle.xyz>
2026-03-25 14:11:54 +0800

    fix: formatted

--- src/protocol/StrategyController.sol
+++ src/protocol/StrategyController.sol
@@ -661,7 +661,8 @@ contract StrategyController is Initializable, AccessControlUpgradeable, Reentran
     // =============================================================
 
     function _invest(uint256 excessCash) internal {
-        uint256 totalAssets = asset.balanceOf(address(vault)) + _totalStrategyValue() + vault.totalInvestInFlight()
+        uint256 totalAssets =
+            asset.balanceOf(address(vault)) + _totalStrategyValue() + vault.totalInvestInFlight()
             + vault.totalRedeemInFlight();
         uint256 requested = excessCash;
         uint256 remaining = excessCash;
